55a368a836f29d40a5c8a22418050696a7fbff8f
MEERA HUSSAIN MORRIMEKALA <112694067+meerahussain733@users.noreply.github.com>
2026-01-28 23:00:27 -0500

    Refactor smoothie API request placement

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,9 @@ ingredients_string=''
 if ingredients_list:
     for fruit_chose in ingredients_list:
         ingredients_string+=fruit_chose +' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df =st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','""" +name_on_order+ """')"""
     time_to_insert = st.button('Submit Orer')
@@ -32,7 +35,4 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df =st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
